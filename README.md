8b18a3209150113a5d49320a046e1e9dc597287c
Michael Xander <michimself@gmail.com>
2018-06-13 22:30:58 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # bye-bye-feedly
-WIP
+
+Export your read later *(formerly saved/favorited)* articles from [feedly](https://feedly.com), and access them from a simple reader.
+
+I kept this minimal, and toolkit/dependency free. The reader has basic styling. Adjust as you like.
+
+## Setup and usage
+
+1. Clone this repo
+2. Add your feedly developer token and user id in `app.py`. You can request them [here](https://feedly.com/v3/auth/dev).
+3. Run `app.py`
+4. Put the created `data.json` and `index.html` on a web server.
+5. Start reading!
+
+## Demo
+
+![bye-bye-feedly Demo](https://i.imgur.com/zHcy4u7.gif)
+
+## Author
+
+* https://michaelxander.com
+* https://twitter.com/michaxndr
---
+++ app.py
@@ -0,0 +1,40 @@
+import io
+import json
+import requests
+import time
+
+
+# Add your feedly API credentials
+user_id = ''
+access_token = ''
+
+
+def get_saved_items(user_id, access_token):
+    headers = {'Authorization' : 'OAuth ' + access_token}
+    url = 'https://cloud.feedly.com/v3/streams/contents?streamId=user/' + user_id + '/tag/global.saved&count=10000'
+
+    print('Requesting saved items…')
+    r = requests.get(url, headers = headers)
+
+    if r.status_code == 200:
+        filename = 'data.json'
+        # For testing feel free to use the below filename instead
+        # filename = 'feedly-saved-' + time.strftime("%Y%m%d-%H%M%S") + '.json'
+        r.encoding = 'UTF-8'
+
+        # Write JSON
+        with io.open(filename, 'w', encoding='UTF-8') as output_file:
+          try:
+            json.dump(r.json(), output_file, indent=4)
+            print('Success: Created ' + filename)
+          except ValueError as error:
+            print(error)
+
+    else:
+        print('Error: Saved items couldn’t be fetched')
+        print('Status code: ' + str(r.status_code))
+        print(r.json())
+        exit(1)
+
+
+get_saved_items(user_id, access_token)
